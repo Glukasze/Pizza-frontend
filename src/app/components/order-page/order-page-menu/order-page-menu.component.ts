785c02216b3734ffa64545df4baa00ea3f3faec2
Glukasze <Mortal^66>
2021-05-02 17:44:10 +0200

    interactive order page -visual adjustments -automatic order summarization

--- src/app/components/order-page/order-page-menu/order-page-menu.component.ts
+++ src/app/components/order-page/order-page-menu/order-page-menu.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Pizza} from '../../../models/pizza';
+import {MenuComponent} from '../../home-page/menu/menu.component';
+import {MenuService} from '../../../services/menu.service';
+import {OrderPageSidepanelComponent} from '../order-page-sidepanel/order-page-sidepanel.component';
+import {OrderPageService} from '../../../services/order-page.service';
 
 @Component({
   selector: 'app-order-page-menu',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderPageMenuComponent implements OnInit {
 
-  constructor() { }
+  pizzas: Pizza[] = [];
+  row1: Pizza[] = [];
+  row2: Pizza[] = [];
+  row3: Pizza[] = [];
+  row4: Pizza[] = [];
+
+  productsPerColumn = 3;
+
+  selectedPizzas: Pizza[] = [];
+
+  constructor(private menuService: MenuService,
+              private orderPageService: OrderPageService) { }
 
   ngOnInit(): void {
+    this.getMenu();
+  }
+
+  getMenu(): void {
+    this.menuService.getMenu()
+      .subscribe(menu => {
+        this.pizzas = menu;
+        if (this.pizzas.length > 0) {
+          this.sortMenu();
+        }
+      });
+  }
+
+  sortMenu(): void {
+    for (let i = 0; i < this.pizzas.length; i++) {
+      if (i < this.productsPerColumn) {
+        this.row1.push(this.pizzas[i]);
+      } else if (i < (this.productsPerColumn * 2)) {
+        this.row2.push(this.pizzas[i]);
+      }
+      else if (i < (this.productsPerColumn * 3)) {
+        this.row3.push(this.pizzas[i]);
+      }
+      else if (i < (this.productsPerColumn * 4)) {
+        this.row4.push(this.pizzas[i]);
+      }
+    }
+  }
+
+  addPizza(pizza: Pizza): void {
+    this.selectedPizzas.push(pizza);
+    this.orderPageService.communicateMessage(this.selectedPizzas);
+  }
+
+  logPizzas(): void {
+    for (const pizza of this.selectedPizzas) {
+      console.log(pizza);
+    }
   }
 
 }
--- src/app/components/order-page/order-page-sidepanel/order-page-sidepanel.component.ts
+++ src/app/components/order-page/order-page-sidepanel/order-page-sidepanel.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit} from '@angular/core';
+import {Pizza} from '../../../models/pizza';
+import {OrderPageService} from '../../../services/order-page.service';
 
 @Component({
   selector: 'app-order-page-sidepanel',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderPageSidepanelComponent implements OnInit {
 
-  constructor() { }
+  selectedPizzas: Pizza[];
+  total = 0;
+
+  constructor(private orderPageService: OrderPageService) { }
 
   ngOnInit(): void {
+    this.orderPageService.sendMessage.subscribe(
+      (pizzas: Pizza[]) => {
+        this.selectedPizzas = pizzas;
+        this.calculateTotal();
+      }
+    );
+  }
+
+  calculateTotal(): void {
+    this.total = 0;
+    for (const pizza of this.selectedPizzas) {
+      // this.total += pizza
+    }
+    // this.total = 0;
+    // if (this.selectedPizzas.length > 0) {
+    //   for (const pizza of this.selectedPizzas) {
+    //     this.total += 10;
+    //     for (const ingredient of pizza.toppings) {
+    //       this.total += ingredient.price;
+    //     }
+    //   }
+    // }
   }
 
 }
